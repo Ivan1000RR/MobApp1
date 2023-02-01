ab9a44f2d38d4b3883a5f669f3c120545932f6d4
Ivan1000RR <ividovic110@gmail.com>
2023-02-01 18:18:19 +0100

    Dodan projekt

---
+++ app/src/main/java/com/example/postalcode/Mjesta.java
@@ -0,0 +1,59 @@
+package com.example.postalcode;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+import java.util.List;
+
+public class Mjesta {
+
+    @SerializedName("post code")
+    private String postCode;
+    private String country;
+    @SerializedName("country abbreviation")
+    private String countryAbbreviation;
+    private List<Place> places;
+
+    public Mjesta(String postCode,String country, String countryAbbreviation, List<Place> places) {
+        this.postCode = postCode;
+        this.country = country;
+        this.countryAbbreviation = countryAbbreviation;
+        this.places = places;
+    }
+
+    public Mjesta(){
+
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryAbbreviation() {
+        return countryAbbreviation;
+    }
+
+    public void setCountryAbbreviation(String countryAbbreviation) {
+        this.countryAbbreviation = countryAbbreviation;
+    }
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<Place> places) {
+        this.places = places;
+    }
+}
